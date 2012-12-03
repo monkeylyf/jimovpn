48b8ff61914203b23cbe9f8b74647eb5e2510dd8
Rubin Xu <Rubin.Xu@cl.cam.ac.uk>
2012-12-02 23:56:38 -0800

    Link auth script to django model.

--- openvpn_hook/client-auth.py
+++ openvpn_hook/client-auth.py
@@ -3,10 +3,17 @@
 import setup
 import os
 import sys
+from vpn_user.models import Users
+from vpn_user.hashers import MyPBKDF2PasswordHasher
 
 def authenticate(username, password):
     remote_ip = os.environ.get("untrusted_ip", "0.0.0.0")
-    return username == "rx201"
+    try:
+        user = Users.objects.get(username__iexact=username)
+        return MyPBKDF2PasswordHasher().verify(password, user.password)
+    except Users.DoesNotExist:
+        pass
+    return False
 
 if __name__ == "__main__":
     username = os.environ.get('username', '')
